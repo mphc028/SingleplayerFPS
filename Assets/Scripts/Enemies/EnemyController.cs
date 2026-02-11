4e30af7777e99595a2759559a092fcf8ec2cda6e
Imanol Valle <mphc028@gmail.com>
2026-02-11 19:33:32 +0100

    Added death anim

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -6,11 +6,15 @@ public class EnemyController : MonoBehaviour
     private Transform player;
     private NavMeshAgent navMeshAgent;
     private int health = 100;
+    private Animator animator;
+
+    private float detectionDistance = 5f;
 
     void Start()
     {
         player = PlayerController.Instance.transform;
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damage, Vector3 hitPosition, Quaternion hitRotation)
@@ -28,8 +32,13 @@ public class EnemyController : MonoBehaviour
     {
         if (player != null)
         {
-            navMeshAgent.SetDestination(new Vector3(player.position.x, transform.position.y, player.position.z));
+            if (Vector3.Distance(transform.position, player.transform.position) < detectionDistance)
+            {
+                navMeshAgent.SetDestination(new Vector3(player.position.x, transform.position.y, player.position.z));
+            }
         }
+
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
     }
 
     private void Die()
